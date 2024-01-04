ed60b0268c3eb87b76ef440c4801444a1b534e79
2wndrhs <2wndrhs@gmail.com>
2024-01-04 21:52:56 +0900

    fix(FrequencyCounter): `same()` 함수 수정

--- FrequencyCounter/same.ts
+++ FrequencyCounter/same.ts
@@ -9,6 +9,7 @@
 // 첫 번째 배열의 모든 값을 제곱한 값을 두 번째 배열이 가지고 있다면 true,
 // 그렇지 안다면 false
 
+// 두 배열의 길이가 다르다면 false 반환
 // 각 배열 안 원소들의 빈도를 저장할 객체를 생성
 // 각 배열을 순회하면서 객체에 원소들의 빈도를 저장
 // 첫 번째 객체를 순회하면서
@@ -17,6 +18,10 @@
 // 첫 번째 객체를 정상적으로 순회했다면, true 반환
 
 const same = (first: number[], second: number[]) => {
+  if (first.length !== second.length) {
+    return false;
+  }
+
   const firstMap = new Map<number, number>();
   const secondMap = new Map<number, number>();
 
