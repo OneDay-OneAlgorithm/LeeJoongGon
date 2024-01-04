556d017517a4ab318f0e01be09af0bedabda5ccd
2wndrhs <2wndrhs@gmail.com>
2024-01-04 21:52:34 +0900

    test(FrequencyCounter): `same()` 함수 테스트 케이스 추가

--- FrequencyCounter/same.test.ts
+++ FrequencyCounter/same.test.ts
@@ -1,6 +1,18 @@
 import same from './same';
 
 describe('same', () => {
+  it('두 배열의 길이가 다를 경우 false 반환', () => {
+    const first = [1, 2, 3];
+    const second = [1, 4];
+    expect(same(first, second)).toBe(false);
+  });
+
+  it('두 배열이 모두 빈 배열일 경우 true 반환', () => {
+    const first: number[] = [];
+    const second: number[] = [];
+    expect(same(first, second)).toBe(true);
+  });
+
   it('두 번째 배열에 동일한 빈도로 첫 번째 배열에 있는 각 요소의 제곱이 포함되면 true 반환', () => {
     const first = [1, 2, 3];
     const second = [1, 4, 9];
@@ -10,12 +22,6 @@ describe('same', () => {
   it('두 번째 배열에 첫 번째 배열과 동일한 빈도로 값이 등장하지만 첫 번째 배열의 제곱이 아닐 경우 false 반환', () => {
     const first = [1, 2, 3];
     const second = [1, 4, 8];
-    expect(same(first, second)).toBe(false);
-  });
-
-  it('두 번째 배열에 첫 번째 배열과 동일하지 않은 빈도로 값이 등장하지만 첫 번째 배열의 제곱일 경우 false 반환', () => {
-    const first = [1, 2, 3];
-    const second = [1, 4, 9, 9];
     expect(same(first, second)).toBe(false);
   });
 });
