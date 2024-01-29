8446b3a914a006c9e965cd325e58ef2a33935a51
2wndrhs <2wndrhs@gmail.com>
2024-01-29 19:51:13 +0900

    test(SinglyLinkedList): `pop()` 메서드 테스트 코드 작성

--- DataStructure/SinglyLinkedList/SinglyLinkedList.test.ts
+++ DataStructure/SinglyLinkedList/SinglyLinkedList.test.ts
@@ -21,4 +21,30 @@ describe('SinglyLinkedList', () => {
       expect(linkedList.length).toBe(3);
     });
   });
+
+  describe('pop', () => {
+    it('연결 리스트의 마지막 노드를 제거하고 반환', () => {
+      const linkedList = new SinglyLinkedList();
+
+      linkedList.push(1);
+      linkedList.push(2);
+      linkedList.push(3);
+
+      const poppedNode = linkedList.pop();
+      expect(poppedNode?.data).toBe(3);
+      expect(linkedList.head?.data).toBe(1);
+      expect(linkedList.tail?.data).toBe(2);
+      expect(linkedList.length).toBe(2);
+    });
+
+    it('연결 리스트가 비어있을 때는 undefined를 반환', () => {
+      const linkedList = new SinglyLinkedList();
+
+      const poppedNode = linkedList.pop();
+      expect(poppedNode).toBeUndefined();
+      expect(linkedList.head).toBeNull();
+      expect(linkedList.tail).toBeNull();
+      expect(linkedList.length).toBe(0);
+    });
+  });
 });
