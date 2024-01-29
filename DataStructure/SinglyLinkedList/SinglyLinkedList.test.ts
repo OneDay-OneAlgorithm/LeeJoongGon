4e908a6fd426bfecf424369f08d40627292727a1
2wndrhs <2wndrhs@gmail.com>
2024-01-29 20:01:44 +0900

    test(SinglyLinkedList): `shift()` 메서드 테스트 코드 작성

--- DataStructure/SinglyLinkedList/SinglyLinkedList.test.ts
+++ DataStructure/SinglyLinkedList/SinglyLinkedList.test.ts
@@ -47,4 +47,30 @@ describe('SinglyLinkedList', () => {
       expect(linkedList.length).toBe(0);
     });
   });
+
+  describe('shift', () => {
+    it('연결 리스트의 첫 번째 노드를 제거하고 반환', () => {
+      const linkedList = new SinglyLinkedList();
+
+      linkedList.push(1);
+      linkedList.push(2);
+      linkedList.push(3);
+
+      const shiftedNode = linkedList.shift();
+      expect(shiftedNode?.data).toBe(1);
+      expect(linkedList.head?.data).toBe(2);
+      expect(linkedList.tail?.data).toBe(3);
+      expect(linkedList.length).toBe(2);
+    });
+
+    it('연결 리스트가 비어있을 때는 undefined를 반환', () => {
+      const linkedList = new SinglyLinkedList();
+
+      const shiftedNode = linkedList.shift();
+      expect(shiftedNode).toBeUndefined();
+      expect(linkedList.head).toBeNull();
+      expect(linkedList.tail).toBeNull();
+      expect(linkedList.length).toBe(0);
+    });
+  });
 });
