d515068d9c83cf549995c9da2190cda08f1b2185
2wndrhs <2wndrhs@gmail.com>
2024-01-29 21:30:11 +0900

    test(SinglyLinkedList): `insert()`, `remove()` 메서드 테스트 코드 작성

--- DataStructure/SinglyLinkedList/SinglyLinkedList.test.ts
+++ DataStructure/SinglyLinkedList/SinglyLinkedList.test.ts
@@ -1,3 +1,4 @@
+import Node from './Node';
 import SinglyLinkedList from './SinglyLinkedList';
 
 describe('SinglyLinkedList', () => {
@@ -26,9 +27,7 @@ describe('SinglyLinkedList', () => {
     it('연결 리스트의 마지막 노드를 제거하고 반환', () => {
       const linkedList = new SinglyLinkedList();
 
-      linkedList.push(1);
-      linkedList.push(2);
-      linkedList.push(3);
+      linkedList.push(1).push(2).push(3);
 
       const poppedNode = linkedList.pop();
       expect(poppedNode?.data).toBe(3);
@@ -52,9 +51,7 @@ describe('SinglyLinkedList', () => {
     it('연결 리스트의 첫 번째 노드를 제거하고 반환', () => {
       const linkedList = new SinglyLinkedList();
 
-      linkedList.push(1);
-      linkedList.push(2);
-      linkedList.push(3);
+      linkedList.push(1).push(2).push(3);
 
       const shiftedNode = linkedList.shift();
       expect(shiftedNode?.data).toBe(1);
@@ -99,9 +96,7 @@ describe('SinglyLinkedList', () => {
     it('인덱스에 해당하는 노드를 반환', () => {
       const linkedList = new SinglyLinkedList();
 
-      linkedList.push(1);
-      linkedList.push(2);
-      linkedList.push(3);
+      linkedList.push(1).push(2).push(3);
 
       const nodeAtIndex1 = linkedList.get(1);
       expect(nodeAtIndex1?.data).toBe(2);
@@ -113,9 +108,7 @@ describe('SinglyLinkedList', () => {
     it('인덱스가 범위를 벗어날 때는 null을 반환', () => {
       const linkedList = new SinglyLinkedList();
 
-      linkedList.push(1);
-      linkedList.push(2);
-      linkedList.push(3);
+      linkedList.push(1).push(2).push(3);
 
       const nodeAtIndex3 = linkedList.get(3);
       expect(nodeAtIndex3).toBeNull();
@@ -129,9 +122,7 @@ describe('SinglyLinkedList', () => {
     it('인덱스에 해당하는 노드의 데이터를 변경', () => {
       const linkedList = new SinglyLinkedList();
 
-      linkedList.push(1);
-      linkedList.push(2);
-      linkedList.push(3);
+      linkedList.push(1).push(2).push(3);
 
       const result = linkedList.set(1, 'updated');
       expect(result).toBe(true);
@@ -141,9 +132,7 @@ describe('SinglyLinkedList', () => {
     it('인덱스가 범위를 벗어날 때는 false를 반환', () => {
       const linkedList = new SinglyLinkedList();
 
-      linkedList.push(1);
-      linkedList.push(2);
-      linkedList.push(3);
+      linkedList.push(1).push(2).push(3);
 
       const result = linkedList.set(3, 'updated');
       expect(result).toBe(false);
@@ -152,6 +141,105 @@ describe('SinglyLinkedList', () => {
       const resultNegative = linkedList.set(-1, 'updated');
       expect(resultNegative).toBe(false);
       expect(linkedList.get(-1)).toBeNull();
+    });
+  });
+
+  describe('insert', () => {
+    it('인덱스에 해당하는 위치에 새로운 노드를 삽입', () => {
+      const linkedList = new SinglyLinkedList();
+
+      linkedList.push(1).push(2).push(3);
+
+      linkedList.insert(1, 'inserted');
+      expect(linkedList.get(1)?.data).toBe('inserted');
+      expect(linkedList.get(2)?.data).toBe(2);
+      expect(linkedList.length).toBe(4);
+    });
+
+    it('인덱스가 범위를 벗어날 때는 false를 반환', () => {
+      const linkedList = new SinglyLinkedList();
+
+      linkedList.push(1).push(2).push(3);
+
+      const result = linkedList.insert(4, 'inserted');
+      expect(result).toBe(false);
+      expect(linkedList.length).toBe(3);
+
+      const resultNegative = linkedList.insert(-1, 'inserted');
+      expect(resultNegative).toBe(false);
+      expect(linkedList.length).toBe(3);
+    });
+
+    it('마지막 인덱스에 노드를 삽입할 경우 push와 동일한 결과를 반환', () => {
+      const linkedList = new SinglyLinkedList();
+
+      linkedList.push(1).push(2).push(3);
+
+      linkedList.insert(3, 'inserted');
+      expect(linkedList.get(3)?.data).toBe('inserted');
+      expect(linkedList.length).toBe(4);
+    });
+
+    it('첫 번째 인덱스에 노드를 삽입할 경우 unshift와 동일한 결과를 반환', () => {
+      const linkedList = new SinglyLinkedList();
+
+      linkedList.push(1).push(2).push(3);
+
+      linkedList.insert(0, 'inserted');
+      expect(linkedList.get(0)?.data).toBe('inserted');
+      expect(linkedList.length).toBe(4);
+    });
+  });
+
+  describe('remove', () => {
+    it('인덱스에 해당하는 노드를 제거하고 반환', () => {
+      const linkedList = new SinglyLinkedList();
+
+      linkedList.push(1).push(2).push(3);
+
+      const removedNode = linkedList.remove(1);
+      expect(removedNode?.data).toBe(2);
+      expect(linkedList.head?.data).toBe(1);
+      expect(linkedList.tail?.data).toBe(3);
+      expect(linkedList.length).toBe(2);
+    });
+
+    it('인덱스가 범위를 벗어날 때는 undefined를 반환', () => {
+      const linkedList = new SinglyLinkedList();
+
+      linkedList.push(1).push(2).push(3);
+
+      const removedNode = linkedList.remove(3);
+      expect(removedNode).toBeUndefined();
+      expect(linkedList.length).toBe(3);
+
+      const removedNodeNegative = linkedList.remove(-1);
+      expect(removedNodeNegative).toBeUndefined();
+      expect(linkedList.length).toBe(3);
+    });
+
+    it('마지막 인덱스의 노드를 제거할 경우 pop와 동일한 결과를 반환', () => {
+      const linkedList = new SinglyLinkedList();
+
+      linkedList.push(1).push(2).push(3);
+
+      const removedNode = linkedList.remove(2);
+      expect(removedNode?.data).toBe(3);
+      expect(linkedList.head?.data).toBe(1);
+      expect(linkedList.tail?.data).toBe(2);
+      expect(linkedList.length).toBe(2);
+    });
+
+    it('첫 번째 인덱스의 노드를 제거할 경우 shift와 동일한 결과를 반환', () => {
+      const linkedList = new SinglyLinkedList();
+
+      linkedList.push(1).push(2).push(3);
+
+      const removedNode = linkedList.remove(0);
+      expect(removedNode?.data).toBe(1);
+      expect(linkedList.head?.data).toBe(2);
+      expect(linkedList.tail?.data).toBe(3);
+      expect(linkedList.length).toBe(2);
     });
   });
 });
