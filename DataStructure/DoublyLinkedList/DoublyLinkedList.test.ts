07208943494f8f7044f940fdd8a2dbfe72662f38
2wndrhs <2wndrhs@gmail.com>
2024-02-04 19:58:25 +0900

    test(DoublyLinkedList): `get()` 메서드 테스트 코드 작성

--- DataStructure/DoublyLinkedList/DoublyLinkedList.test.ts
+++ DataStructure/DoublyLinkedList/DoublyLinkedList.test.ts
@@ -80,4 +80,25 @@ describe('DoubleLinkedList', () => {
       expect(linkedList.length).toBe(3);
     });
   });
+
+  describe('get', () => {
+    it('특정 인덱스의 노드를 반환', () => {
+      const linkedList = new DoublyLinkedList();
+
+      linkedList.push(1).push(2).push(3);
+
+      expect(linkedList.get(0)?.data).toBe(1);
+      expect(linkedList.get(1)?.data).toBe(2);
+      expect(linkedList.get(2)?.data).toBe(3);
+    });
+
+    it('인덱스가 범위를 벗어날 경우에는 null 반환', () => {
+      const linkedList = new DoublyLinkedList();
+
+      linkedList.push(1).push(2).push(3);
+
+      expect(linkedList.get(-1)).toBeNull();
+      expect(linkedList.get(3)).toBeNull();
+    });
+  });
 });
