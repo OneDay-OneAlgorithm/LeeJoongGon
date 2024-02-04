80df774c2574da7945cb0f141cfd886002e316de
2wndrhs <2wndrhs@gmail.com>
2024-02-04 19:37:33 +0900

    test(DoublyLinkedList): `shift()` 메서드 테스트 코드 작성

--- DataStructure/DoublyLinkedList/DoublyLinkedList.test.ts
+++ DataStructure/DoublyLinkedList/DoublyLinkedList.test.ts
@@ -13,31 +13,57 @@ describe('DoubleLinkedList', () => {
       expect(linkedList.tail?.prev?.data).toBe(2);
       expect(linkedList.length).toBe(3);
     });
+  });
 
-    describe('pop', () => {
-      it('연결 리스트의 마지막 노드를 제거하고 반환', () => {
-        const linkedList = new DoublyLinkedList();
+  describe('pop', () => {
+    it('연결 리스트의 마지막 노드를 제거하고 반환', () => {
+      const linkedList = new DoublyLinkedList();
 
-        linkedList.push(1).push(2).push(3);
+      linkedList.push(1).push(2).push(3);
 
-        const poppedNode = linkedList.pop();
-        expect(poppedNode?.data).toBe(3);
-        expect(linkedList.head?.data).toBe(1);
-        expect(linkedList.head?.next?.data).toBe(2);
-        expect(linkedList.tail?.data).toBe(2);
-        expect(linkedList.tail?.prev?.data).toBe(1);
-        expect(linkedList.length).toBe(2);
-      });
+      const poppedNode = linkedList.pop();
+      expect(poppedNode?.data).toBe(3);
+      expect(linkedList.head?.data).toBe(1);
+      expect(linkedList.head?.next?.data).toBe(2);
+      expect(linkedList.tail?.data).toBe(2);
+      expect(linkedList.tail?.prev?.data).toBe(1);
+      expect(linkedList.length).toBe(2);
+    });
 
-      it('연결 리스트가 비어있을 때는 undefined를 반환', () => {
-        const linkedList = new DoublyLinkedList();
+    it('연결 리스트가 비어있을 때는 undefined를 반환', () => {
+      const linkedList = new DoublyLinkedList();
 
-        const poppedNode = linkedList.pop();
-        expect(poppedNode).toBeUndefined();
-        expect(linkedList.head).toBeNull();
-        expect(linkedList.tail).toBeNull();
-        expect(linkedList.length).toBe(0);
-      });
+      const poppedNode = linkedList.pop();
+      expect(poppedNode).toBeUndefined();
+      expect(linkedList.head).toBeNull();
+      expect(linkedList.tail).toBeNull();
+      expect(linkedList.length).toBe(0);
+    });
+  });
+
+  describe('shift', () => {
+    it('연결 리스트의 첫 번째 노드를 제거하고 반환', () => {
+      const linkedList = new DoublyLinkedList();
+
+      linkedList.push(1).push(2).push(3);
+
+      const shiftedNode = linkedList.shift();
+      expect(shiftedNode?.data).toBe(1);
+      expect(linkedList.head?.data).toBe(2);
+      expect(linkedList.head?.next?.data).toBe(3);
+      expect(linkedList.tail?.data).toBe(3);
+      expect(linkedList.tail?.prev?.data).toBe(2);
+      expect(linkedList.length).toBe(2);
+    });
+
+    it('연결 리스트가 비어있을 때는 undefined를 반환', () => {
+      const linkedList = new DoublyLinkedList();
+
+      const shiftedNode = linkedList.shift();
+      expect(shiftedNode).toBeUndefined();
+      expect(linkedList.head).toBeNull();
+      expect(linkedList.tail).toBeNull();
+      expect(linkedList.length).toBe(0);
     });
   });
 });
