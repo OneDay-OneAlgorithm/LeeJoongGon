abe29da642436f2579dd0b5b5c3b60349e9d7e4d
2wndrhs <2wndrhs@gmail.com>
2024-07-16 03:17:36 +0900

    test(BST): `insert()` 메서드 테스트 케이스 추가, `find()` 메서드 테스트 코드 추가

--- DataStructure/Tree/BinarySearchTree.test.ts
+++ DataStructure/Tree/BinarySearchTree.test.ts
@@ -7,13 +7,59 @@ describe('BinarySearchTree', () => {
     tree = new BinarySearchTree();
   });
 
-  it('insert() 메서드는 이진 탐색 트리에 노드를 추가', () => {
-    tree.insert(8);
-    tree.insert(7);
-    tree.insert(9);
+  describe('insert()', () => {
+    it('루트 노드가 없으면 루트 노드로 추가', () => {
+      tree.insert(10);
 
-    expect(tree.root?.value).toBe(8);
-    expect(tree.root?.left?.value).toBe(7);
-    expect(tree.root?.right?.value).toBe(9);
+      expect(tree.root?.value).toBe(10);
+    });
+
+    it('루트 노드가 있으면 적절한 위치에 추가', () => {
+      tree.insert(10);
+      tree.insert(5);
+      tree.insert(15);
+
+      expect(tree.root?.value).toBe(10);
+      expect(tree.root?.left?.value).toBe(5);
+      expect(tree.root?.right?.value).toBe(15);
+    });
+
+    it('중복된 값을 추가하면 null을 반환', () => {
+      tree.insert(10);
+      tree.insert(5);
+      tree.insert(15);
+      tree.insert(5);
+
+      expect(tree.root?.value).toBe(10);
+      expect(tree.root?.left?.value).toBe(5);
+      expect(tree.root?.right?.value).toBe(15);
+      expect(tree.root?.left?.left).toBeNull();
+    });
+  });
+
+  describe('find()', () => {
+    it('루트 노드가 없으면 null을 반환', () => {
+      expect(tree.find(10)).toBeNull();
+    });
+
+    it('값을 찾으면 해당 노드를 반환', () => {
+      tree.insert(10);
+      tree.insert(5);
+      tree.insert(15);
+
+      const node = tree.find(5);
+
+      expect(node?.value).toBe(5);
+    });
+
+    it('값을 찾지 못하면 null을 반환', () => {
+      tree.insert(10);
+      tree.insert(5);
+      tree.insert(15);
+
+      const node = tree.find(20);
+
+      expect(node).toBeNull();
+    });
   });
 });
